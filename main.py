e918a62da808d57231da1b7e1b72e193e5c5d9e7
Carter <39602818+Carter2506@users.noreply.github.com>
2019-11-30 17:51:29 +0000

    Added support for both windows and linux
    Automatically checks OS to use the right code. Also removed Temp folder from needing to exist. Now the script can be run with only 'main.py' and '/files'.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import pickle
+import shutil
 import textwrap
 import requests
 import fortnite_api
@@ -22,7 +23,7 @@ def watermark_text(input_image_path, output_image_path, text, pos, textSize=26, 
 	# make the image editable
 	drawing = ImageDraw.Draw(photo)
 
-	font = ImageFont.truetype("Temp/font.ttf", textSize) ##font used in game
+	font = ImageFont.truetype("files/font.ttf", textSize) ##font used in game
 	drawing.text(pos, text, fill=color, font=font, fontsize=5)
 	photo.save(output_image_path)
 
@@ -42,64 +43,155 @@ def text_wrap_overlay(bodyNum, posX, yOff=608, outname='Temp/output/brNews.png')
 		watermark_text('Temp/output/brNews.png', outname, line, pos=(posX, yOff))
 		yOff = yOff + 29
 
-try:
-	os.mkdir('/tmp/fnNews/')
-except:
-	print('[WARN] /tmp/fnNews already exists. Skipping')
-
-while True: ##loop to check for change. Delay: 100s
-	links = []; title = []; body = []; chars = [] ##resets lists
-
+if os.name == 'nt': ##Check for windows or linux
+	print('[WARN] Windows Detected. Switching to windows code')
 	try:
-		with open('/tmp/fnNews/news_old' + '.pkl', 'rb') as f: ##pickle load
-			news_old = pickle.load(f)
-	except:
-		print('[WARN] .pkl file doesn\'t exist. Skipping')
+		os.mkdir('Temp/')
+		os.mkdir('Temp/output/')
+	except FileExistsError:
+		print('[WARN] Temp/ already exists. Skipping')
 
-	if news_raw != news_old: ##check for updates
-		print('[ALERT] In-game news updated')
-
-		with open('/tmp/fnNews/news_old' + '.pkl', 'wb') as f: ##new pickle dump. to be compared next iteration
-		    pickle.dump(news_raw, f, pickle.HIGHEST_PROTOCOL)
-
-		x = 0
-		for i in news_raw['messages']:
-			title.append(news_raw['messages'][x]['title']) ##adds the data to lists
-			links.append(news_raw['messages'][x]['image'])
-			body.append(news_raw['messages'][x]['body'])
-			x = x + 1
-
-		x = 0; f = ''
-		for i in links: ##downloads indervidual images
-			url = links[x]
-			response = requests.get(url)
-			if response.status_code == 200:
-				with open("Temp/newsImage"+str(int(x+1))+'.jpg', 'wb') as f:
-					f.write(response.content)
-					resize(520, 'Temp/newsImage'+str(x+1)+'.jpg', 'Temp/newsImage'+str(x+1)+'.jpg')
-					chars.append(str(len(body[int(x)].split()))) ##unrelated line but it works here -- This is for getting the length of the main body of the text.
-			x = x + 1
-
-		watermark_photo('Temp/pic.png', 'Temp/output/brNews.png', 'Temp/newsImage1.jpg', position=(115,289))
-		watermark_photo('Temp/output/brNews.png', 'Temp/output/brNews.png', 'Temp/newsImage2.jpg', position=(701,289))
-		watermark_photo('Temp/output/brNews.png', 'Temp/output/brNews.png', 'Temp/newsImage3.jpg', position=(1286,289)) ##overlays indervidual 3 images over main template
-
-		watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[0], pos=(124, 561), textSize=38, color=(0, 0, 0))
-		watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[1], pos=(709, 561), textSize=38, color=(0, 0, 0))
-		watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[2], pos=(1295, 561), textSize=38, color=(0, 0, 0)) ##overlays text to template
-
-		text_wrap_overlay(0, 124)
-		text_wrap_overlay(1, 709)
-		text_wrap_overlay(2, 1295) ##overlays main body text to template
-
-		os.rename('Temp/output/brNews.png', 'webDir/news.png') ##moves to main directory
-
-		print('[ALERT] Process Finished') ##Nice
+	while True: ##loop to check for change. Delay: 100s
+		links = []; title = []; body = []; chars = [] ##resets lists
 
 		try:
-			os.remove('Temp/newsImage1.jpg'); os.remove('Temp/newsImage2.jpg'); os.remove('Temp/newsImage2.jpg'); os.remove('Temp/newsImage4.jpg'); os.remove('Temp/newsImage5.jpg'); os.remove('Temp/newsImage6.jpg'); os.remove('Temp/final.png')
-			print('[ALERT] Cleaning Files Complete')
+			os.mkdir('Temp/')
+			os.mkdir('Temp/output/')
+		except FileExistsError:
+			pass
+
+		try:
+			with open('Temp/news_old' + '.pkl', 'rb') as f: ##pickle load
+				news_old = pickle.load(f)
 		except:
-			print('[WARN] Cleaning Files Failed') ##this doesn't work most of the time idk why
-	else:
-		time.sleep(100)
+			print('[WARN] .pkl file doesn\'t exist. Skipping')
+			news_old = 'nopickle'
+
+		if news_raw != news_old: ##check for updates
+			print('[ALERT] In-game news updated')
+
+			with open('Temp/news_old' + '.pkl', 'wb') as f: ##new pickle dump. to be compared next iteration
+			    pickle.dump(news_raw, f, pickle.HIGHEST_PROTOCOL)
+
+			x = 0
+			for i in news_raw['messages']:
+				title.append(news_raw['messages'][x]['title']) ##adds the data to lists
+				links.append(news_raw['messages'][x]['image'])
+				body.append(news_raw['messages'][x]['body'])
+				x = x + 1
+
+			x = 0; f = ''
+			for i in links: ##downloads indervidual images
+				url = links[x]
+				response = requests.get(url)
+				if response.status_code == 200:
+					with open("Temp/newsImage"+str(int(x+1))+'.jpg', 'wb') as f:
+						f.write(response.content)
+						resize(520, 'Temp/newsImage'+str(x+1)+'.jpg', 'Temp/newsImage'+str(x+1)+'.jpg')
+						chars.append(str(len(body[int(x)].split()))) ##unrelated line but it works here -- This is for getting the length of the main body of the text.
+				x = x + 1
+
+			watermark_photo('files/template.png', 'Temp/output/brNews.png', 'Temp/newsImage1.jpg', position=(115,289))
+			watermark_photo('Temp/output/brNews.png', 'Temp/output/brNews.png', 'Temp/newsImage2.jpg', position=(701,289))
+			watermark_photo('Temp/output/brNews.png', 'Temp/output/brNews.png', 'Temp/newsImage3.jpg', position=(1286,289)) ##overlays indervidual 3 images over main template
+
+			watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[0], pos=(124, 561), textSize=38, color=(0, 0, 0))
+			watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[1], pos=(709, 561), textSize=38, color=(0, 0, 0))
+			watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[2], pos=(1295, 561), textSize=38, color=(0, 0, 0)) ##overlays text to template
+
+			text_wrap_overlay(0, 124)
+			text_wrap_overlay(1, 709)
+			text_wrap_overlay(2, 1295) ##overlays main body text to template
+
+			try:
+				os.rename('Temp/output/brNews.png', 'news.png') ##moves to main directory
+			except FileExistsError:
+				os.remove('news.png')
+				os.rename('Temp/output/brNews.png', 'news.png')
+
+			print('[ALERT] Process Finished') ##Nice
+
+			try:
+				shutil.rmtree('Temp')
+				print('[ALERT] Cleaning Files Complete')
+			except:
+				print('[WARN] Cleaning Files Failed') ##this doesn't work most of the time idk why
+		else:
+			time.sleep(100)
+
+else:
+	try:
+		os.mkdir('/tmp/fnNews/')
+	except FileExistsError:
+		print('[WARN] /tmp/fnNews already exists. Skipping')
+	try:
+		os.mkdir('Temp/')
+		os.mkdir('Temp/output/')
+	except FileExistsError:
+		print('[WARN] Temp/ already exists. Skipping')
+
+	while True: ##loop to check for change. Delay: 100s
+		links = []; title = []; body = []; chars = [] ##resets lists
+		try:
+			os.mkdir('Temp/')
+			os.mkdir('Temp/output/')
+		except:
+			pass
+
+		try:
+			with open('/tmp/fnNews/news_old' + '.pkl', 'rb') as f: ##pickle load
+				news_old = pickle.load(f)
+		except:
+			print('[WARN] .pkl file doesn\'t exist. Skipping')
+			news_old = 'nopickle'
+
+		if news_raw != news_old: ##check for updates
+			print('[ALERT] In-game news updated')
+
+			with open('/tmp/fnNews/news_old' + '.pkl', 'wb') as f: ##new pickle dump. to be compared next iteration
+			    pickle.dump(news_raw, f, pickle.HIGHEST_PROTOCOL)
+
+			x = 0
+			for i in news_raw['messages']:
+				title.append(news_raw['messages'][x]['title']) ##adds the data to lists
+				links.append(news_raw['messages'][x]['image'])
+				body.append(news_raw['messages'][x]['body'])
+				x = x + 1
+
+			x = 0; f = ''
+			for i in links: ##downloads indervidual images
+				url = links[x]
+				response = requests.get(url)
+				if response.status_code == 200:
+					with open("Temp/newsImage"+str(int(x+1))+'.jpg', 'wb') as f:
+						f.write(response.content)
+						resize(520, 'Temp/newsImage'+str(x+1)+'.jpg', 'Temp/newsImage'+str(x+1)+'.jpg')
+						chars.append(str(len(body[int(x)].split()))) ##unrelated line but it works here -- This is for getting the length of the main body of the text.
+				x = x + 1
+
+			watermark_photo('files/template.png', 'Temp/output/brNews.png', 'Temp/newsImage1.jpg', position=(115,289))
+			watermark_photo('Temp/output/brNews.png', 'Temp/output/brNews.png', 'Temp/newsImage2.jpg', position=(701,289))
+			watermark_photo('Temp/output/brNews.png', 'Temp/output/brNews.png', 'Temp/newsImage3.jpg', position=(1286,289)) ##overlays indervidual 3 images over main template
+
+			watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[0], pos=(124, 561), textSize=38, color=(0, 0, 0))
+			watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[1], pos=(709, 561), textSize=38, color=(0, 0, 0))
+			watermark_text('Temp/output/brNews.png', 'Temp/output/brNews.png', title[2], pos=(1295, 561), textSize=38, color=(0, 0, 0)) ##overlays text to template
+
+			text_wrap_overlay(0, 124)
+			text_wrap_overlay(1, 709)
+			text_wrap_overlay(2, 1295) ##overlays main body text to template
+
+			try:
+				os.rename('Temp/output/brNews.png', 'news.png') ##moves to main directory
+			except:
+				os.remove('news.png')
+				os.rename('Temp/output/brNews.png', 'news.png')
+			print('[ALERT] Process Finished') ##Nice
+
+			try:
+				shutil.rmtree('Temp')
+				print('[ALERT] Cleaning Files Complete')
+			except:
+				print('[WARN] Cleaning Files Failed') ##this doesn't work most of the time idk why
+		else:
+			time.sleep(100)
